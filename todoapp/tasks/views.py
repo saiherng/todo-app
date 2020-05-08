d7efaefe9711f26caf2a2f971297fd8deb365616
Sai Herng <ssthfungsai.tom@gmail.com>
2020-05-08 00:34:08 -0500

    added a basic to do functionality

--- todoapp/tasks/views.py
+++ todoapp/tasks/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import *
+from .forms import *
 
 def index(request):
-    return HttpResponse("This is a ToDo-App!")
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect('/')
+
+    context = {
+                'tasks' : tasks,
+                'form' : form
+                }
+
+    return render(request, 'tasks/task_list.html', context)
